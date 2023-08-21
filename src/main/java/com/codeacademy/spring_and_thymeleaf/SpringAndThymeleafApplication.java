4bff0b22b4092deba0502e0b3c82af900a70c3f8
Mantas Sasnauskas <mantas.sasnauskas@gmail.com>
2023-08-21 21:15:08 +0300

    First commit

---
+++ src/main/java/com/codeacademy/spring_and_thymeleaf/SpringAndThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.codeacademy.spring_and_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndThymeleafApplication.class, args);
+	}
+
+}
